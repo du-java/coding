b58016a7d60a573a959dfec6b7967fff55eecf7a
AlexOak <dubovec995@gmail.com>
2021-06-29 21:24:37 +0300

    wf

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,7 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
- * You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even
+ * You will be given an array of numbers. You have to sort the odd numbers in
+ * ascending order while leaving the even
  * numbers at their original positions.
- *
+ * <p>
  * Examples
  * <p>
  * [7, 1]  =>  [1, 7]
@@ -11,6 +16,20 @@
 
 public class Kata {
     public static int[] sortArray(int[] array) {
+        List<Integer> oddList = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0){
+                oddList.add(array[i]);
+                array[i] = -1;
+            }
+        }
+        Collections.sort(oddList);
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i] == -1){
+                array[i] = oddList.get(0);
+                oddList.remove(0);
+            }
+        }
         return array;
     }
 }
