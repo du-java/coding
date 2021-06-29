2b9d037d772080bc09917256d4d930539bb42db1
Dimacom <lqs5283829>
2021-06-29 19:44:41 +0300

    new commit

--- src/main/java/Prime.java
+++ src/main/java/Prime.java
@@ -17,6 +17,12 @@
 
 public class Prime {
     public static boolean isPrime(int num) {
-        return false; //TODO
+        if (num < 2) return false;
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
