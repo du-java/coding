3b1185467b7d8df2fe536750366551d95d9b79a3
AlexOak <dubovec995@gmail.com>
2021-06-29 21:48:42 +0300

    wf

--- src/main/java/CamelCaseMethod.java
+++ src/main/java/CamelCaseMethod.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 /**
  * Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
  * <p>
@@ -8,7 +10,19 @@
  */
 public class CamelCaseMethod {
     public static String camelCase(String str) {
-        // your code here
-        return null;
+        String[] s = str.split("\\s+");
+        String word = "";
+        String camelCase = "";
+        for (int i = 0; i < s.length; i++) {
+            if (s[i].isEmpty()){
+                continue;
+            }
+            word = s[i];
+            String firstLetter = s[i].substring(0, 1).toUpperCase(Locale.ROOT);
+            String substring = word.substring(1);
+            String rtn = firstLetter + substring;
+            camelCase += rtn;
+        }
+        return camelCase;
     }
 }
