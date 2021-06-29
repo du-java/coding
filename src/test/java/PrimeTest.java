0ac6d17c13468c861288962e7a06ee372108f724
du_java <du_java@bk.ru>
2021-06-29 18:17:40 +0200

    fixed tests

--- src/test/java/PrimeTest.java
+++ src/test/java/PrimeTest.java
@@ -7,32 +7,32 @@ public class PrimeTest {
 
     @Test
     public void testBasic() {
-        assertFalse("0  is not prime", Prime.isPrime(0));
-        assertFalse("1  is not prime", Prime.isPrime(1));
-        assertTrue ("2  is prime",     Prime.isPrime(2));
-        assertTrue ("73 is prime",     Prime.isPrime(73));
-        assertFalse("75 is not prime", Prime.isPrime(75));
-        assertFalse("-1 is not prime", Prime.isPrime(-1));
+        assertFalse(Prime.isPrime(0), "0  is not prime");
+        assertFalse(Prime.isPrime(1), "1  is not prime");
+        assertTrue(Prime.isPrime(2), "2  is prime");
+        assertTrue(Prime.isPrime(73), "73 is prime");
+        assertFalse(Prime.isPrime(75), "75 is not prime");
+        assertFalse(Prime.isPrime(-1), "-1 is not prime");
     }
 
     @Test
     public void testPrime() {
-        assertTrue("3 is prime", Prime.isPrime(3));
-        assertTrue("5 is prime", Prime.isPrime(5));
-        assertTrue("7 is prime", Prime.isPrime(7));
-        assertTrue("41 is prime", Prime.isPrime(41));
-        assertTrue("5099 is prime", Prime.isPrime(5099));
+        assertTrue(Prime.isPrime(3), "3 is prime");
+        assertTrue(Prime.isPrime(5), "5 is prime");
+        assertTrue(Prime.isPrime(7), "7 is prime");
+        assertTrue(Prime.isPrime(41), "41 is prime");
+        assertTrue(Prime.isPrime(5099), "5099 is prime");
     }
 
     @Test
     public void testNotPrime() {
-        assertFalse("4 is not prime", Prime.isPrime(4));
-        assertFalse("6 is not prime", Prime.isPrime(6));
-        assertFalse("8 is not prime", Prime.isPrime(8));
-        assertFalse("9 is not prime", Prime.isPrime(9));
-        assertFalse("45 is not prime", Prime.isPrime(45));
-        assertFalse("-5 is not prime", Prime.isPrime(-5));
-        assertFalse("-8 is not prime", Prime.isPrime(-8));
-        assertFalse("-41 is not prime", Prime.isPrime(-41));
+        assertFalse(Prime.isPrime(4), "4 is not prime");
+        assertFalse(Prime.isPrime(6), "6 is not prime");
+        assertFalse(Prime.isPrime(8), "8 is not prime");
+        assertFalse(Prime.isPrime(9), "9 is not prime");
+        assertFalse(Prime.isPrime(45), "45 is not prime");
+        assertFalse(Prime.isPrime(-5), "-5 is not prime");
+        assertFalse(Prime.isPrime(-8), "-8 is not prime");
+        assertFalse(Prime.isPrime(-41), "-41 is not prime");
     }
 }
