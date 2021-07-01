32eff6d6964715d841cabbf41cbc157ac2d8e277
du_java <du_java@bk.ru>
2021-07-01 18:15:29 +0200

    codeing

---
+++ src/test/java/DeadFishTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeadFishTest {
+
+    @Test
+    public void exampleTests() {
+        assertArrayEquals(new int[]{8, 64}, DeadFish.parse("iiisdoso"));
+        assertArrayEquals(new int[]{8, 64, 3600}, DeadFish.parse("iiisdosodddddiso"));
+    }
+}
