ae1257b2484218f99ac1a56948753a6b4d5cc328
AlexOak <dubovec995@gmail.com>
2021-07-01 22:01:25 +0300

    wf

--- src/main/java/ObservedPin.java
+++ src/main/java/ObservedPin.java
@@ -39,7 +39,23 @@ import java.util.List;
  */
 
 public class ObservedPin {
+
     public static List<String> getPINs(String observed) {
+
+
+
+
+
+
+// * ┌───┬───┬───┐
+// * │ 1 │ 2 │ 3 │
+// * ├───┼───┼───┤
+// * │ 4 │ 5 │ 6 │
+// * ├───┼───┼───┤
+// * │ 7 │ 8 │ 9 │
+// * └───┼───┼───┘
+// *     │ 0 │
+// *     └───┘
         return null;
-    } // getPINs
+    }
 }
--- src/main/java/StripComments.java
+++ src/main/java/StripComments.java
@@ -1,29 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Given an input string of:
- *
+ * <p>
  * apples, pears # and bananas
  * grapes
  * bananas !apples
- *
+ * <p>
  * The output expected would be:
- *
+ * <p>
  * apples, pears
  * grapes
  * bananas
- *
+ * <p>
  * The code would be called like so:
- *
+ * <p>
  * var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
  * // result should == "apples, pears\ngrapes\nbananas"
  */
 public class StripComments {
 
     public static String stripComments(String text, String[] commentSymbols) {
-        return "";
+        List<String> list = new ArrayList<String>();
+        String[] split = text.split("\n");
+        for (String s : split) {
+            for (String s1 : commentSymbols) {
+                int i = s.indexOf(s1);
+                if (i != -1) {
+                    s = s.substring(0, i).trim();
+                }
+            }
+
+            list.add(s);
+        }
+//        "apples, pears\ngrapes\nbananas",
+//                StripComments.stripComments("apples, pears # and bananas\ngrapes\nbananas !apples", new String[]{"#", "!"})
+//      
+//                "a\nc\nd",
+//                StripComments.stripComments("a #b\nc\nd $e f g", new String[]{"#", "$"})
+
+        return String.join("\n",list);
     }
 
 }
