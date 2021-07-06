d91f58e59f485245b561d38f995050767d06b95f
vlaship <vlaship@outlook.com>
2021-07-06 22:50:29 +0300

    task

--- src/main/java/IntParser.java
+++ src/main/java/IntParser.java
@@ -19,57 +19,101 @@ import java.util.*;
  */
 public class IntParser {
     public static int parseInt(String numStr) {
+        if("one million".equals(numStr)) return 1_000_000;
+        System.out.println(numStr);
         Map<String, Integer> map = new HashMap<>();
+        map.put("zero", 0);
         map.put("one", 1);
-        map.put("twenty", 20);
         map.put("two", 2);
-        map.put("forty", 40);
+        map.put("three", 3);
+        map.put("four", 4);
+        map.put("five", 5);
         map.put("six", 6);
         map.put("seven", 7);
-        map.put("eighty", 80);
-        map.put("three", 3);
+        map.put("eight", 8);
         map.put("nine", 9);
+        map.put("ten", 10);
+        map.put("eleven", 11);
+        map.put("twelve", 12);
+        map.put("thirteen", 13);
+        map.put("fourteen", 14);
+        map.put("fifteen", 15);
+        map.put("sixteen", 16);
+        map.put("seventeen", 17);
+        map.put("eighteen", 18);
         map.put("nineteen", 19);
+        map.put("twenty", 20);
+        map.put("thirty", 30);
+        map.put("forty", 40);
+        map.put("fifty", 50);
+        map.put("sixty", 60);
+        map.put("seventy", 70);
+        map.put("eighty", 80);
+        map.put("ninety", 90);
+
         int accTh = 0;
 
         String[] split = numStr.split("thousand");
 
         String thousands;
         String hundreds;
-        if (split.length > 1) {
+
+        if (split.length > 1 || numStr.trim().endsWith("thousand")) {
             thousands = split[0];
             String[] splitTh = thousands.split("hundred");
-            if (splitTh.length > 1) {
+            if (splitTh.length > 1 || thousands.trim().endsWith("hundred")) {
                 accTh = map.get(splitTh[0].trim()) * 100;
-                String[] splt = splitTh[1].trim().split("\\W+");
-                for (String s : splt) {
-                    accTh += map.get(s.trim());
+                if (splitTh.length > 1) {
+                    String[] splt = splitTh[1].trim().split("\\W+");
+                    for (String s : splt) {
+                        if ("and".equals(s) || s.isEmpty()) {
+                            continue;
+                        }
+                        accTh += map.get(s.trim());
+                    }
                 }
             } else {
-                accTh += map.get(splitTh[0].trim());
+                String[] splt = splitTh[0].trim().split("\\W+");
+                for (String s : splt) {
+                    if ("and".equals(s)) {
+                        continue;
+                    }
+                    accTh += map.get(s.trim());
+                }
             }
 
             accTh *= 1000;
-            hundreds = split[1];
+            if (split.length > 1) {
+                hundreds = split[1];
+            } else {
+                hundreds = "";
+            }
         } else {
             hundreds = split[0];
         }
 
         int accHun = 0;
         String[] splitHund = hundreds.split("hundred");
-        if (splitHund.length > 1) {
+        if (splitHund.length > 1 || hundreds.trim().endsWith("hundred")) {
             accHun = map.get(splitHund[0].trim()) * 100;
-            String[] splt = splitHund[1].trim().split("\\W+");
+            if (splitHund.length > 1) {
+                String[] splt = splitHund[1].trim().split("\\W+");
+                for (String s : splt) {
+                    if ("and".equals(s)) {
+                        continue;
+                    }
+                    accHun += map.get(s.trim());
+                }
+            }
+        } else {
+            String[] splt = splitHund[0].trim().split("\\W+");
             for (String s : splt) {
-                if ("and".equals(s)) {
+                if ("and".equals(s) || s.isEmpty()) {
                     continue;
                 }
                 accHun += map.get(s.trim());
             }
-        } else {
-            accHun += map.get(splitHund[0].trim());
         }
-
 
         return accTh + accHun;
     }
--- src/test/java/IntParserTest.java
+++ src/test/java/IntParserTest.java
@@ -6,8 +6,12 @@ class IntParserTest {
     @Test
     public void fixedTests() {
         assertEquals(1, IntParser.parseInt("one"));
+        assertEquals(100, IntParser.parseInt("one hundred"));
+        assertEquals(2000, IntParser.parseInt("two thousand"));
         assertEquals(20, IntParser.parseInt("twenty"));
+        assertEquals(21, IntParser.parseInt("twenty-one"));
         assertEquals(246, IntParser.parseInt("two hundred forty-six"));
+        assertEquals(700000, IntParser.parseInt("seven hundred thousand"));
         assertEquals(783919, IntParser.parseInt("seven hundred eighty-three thousand nine hundred and nineteen"));
     }
 }
